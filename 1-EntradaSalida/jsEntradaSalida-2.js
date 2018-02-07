26238354018315c1ae4eeab0d28afaaafc488358
elijanaemilyson <36230674+elijanaemilyson@users.noreply.github.com>
2018-02-07 12:35:02 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -1,7 +1,13 @@
 /*Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function Mostrar()
-{
+{ 
+    //declaro una variable siempre al principio  
+    var dato; 
+   
+    //pido algun dato para ingresar 
+    dato = prompt ("Ingrese un dato");
 	
+    // muestra el contenido de esa variable que es dato 
+    alert(dato);
 }
-
--- 1-EntradaSalida/jsEntradaSalida-3.js
+++ 1-EntradaSalida/jsEntradaSalida-3.js
@@ -2,7 +2,18 @@
 y luego mostrarlo por 'Alert' al presionar el botón  'MOSTRAR'*/
 function Mostrar()
 {
-	
+	//declaro la variable 
+
+    var nombre;
+
+    // uno el documento Html por la caja de texto  
+    nombre=document.getElementById("elNombre").value;
+
+    //cierro con la variable para indicar el nombre
+
+    alert (nombre); 
+
+     
 }
 
 
