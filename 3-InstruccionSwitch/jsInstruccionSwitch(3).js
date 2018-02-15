d8da30ff21ef9592a8b61c066d4db81c32b6fe77
elijanaemilyson <36230674+elijanaemilyson@users.noreply.github.com>
2018-02-15 10:55:44 -0300

    ultimo que hice pero falta uno

--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -5,6 +5,16 @@ var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
 	
+    switch (mesDelAño){
+        case "Febrero":
+        alert("Este mes no tiene mas de 29 días");
+        break;
+
+        default:
+        alert("Este mes tiene 30 días o más ");
+        break;
+    }
+    
 	
 
 
--- 3-InstruccionSwitch/jsInstruccionSwitch(6).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(6).js
@@ -3,6 +3,45 @@ function Mostrar()
 //tomo la hora
 var laHora = document.getElementById('hora').value;
 
+/* La otra forma de resolver el problema es convertir los datos por parseInt para que me agarre los datos como numeros,
+y no en cadena .En este caso solo quito las comillas */
+
+ switch (laHora){
+        case "7":
+        case "8":
+        case "9":
+        case "10":
+        case "11":
+        alert("Es de mañana");
+        break;
+
+        case "12":
+        case "13":
+        case "14":
+        case "15":
+        case "16":
+        case "17":
+        case "18":
+        case "19":
+        alert("Es de tarde");
+        break;
+
+        case "20":
+        case "21":
+        case "22":
+        case "23":
+        case "24":
+        alert("Es de noche");
+        break;
+
+        default: 
+        alert("La hora no existe");
+        break;
+
+     
+
+}
+
 
 
 }//FIN DE LA FUNCIÓN
