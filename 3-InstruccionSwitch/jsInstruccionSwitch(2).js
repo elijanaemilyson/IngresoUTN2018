43b664f93e794603339f43a2d28f439984d88db3
elijanaemilyson <36230674+elijanaemilyson@users.noreply.github.com>
2018-02-16 22:06:10 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -3,7 +3,41 @@ function Mostrar()
 //tomo el mes 
 var mesDelAño = document.getElementById('mes').value;
 
-alert (mesDelAño);
+switch (mesDelAño)
+
+{
+    case "Julio":
+    
+    case "Agosto":
+    alert("Abrigate que hace frio");
+    break;
+
+    case "Septiembre":
+
+    case "Octubre":
+
+    case "Noviembre":
+
+    case "Diciembre":
+    alert("Ya pasamos el frio , ahora calor");
+    break;
+
+    case "Enero":
+
+    case "Febrero":
+
+    case "Marzo":
+
+    case "Abril":
+
+    case "Mayo":
+
+    case "Junio":
+    alert("Falta para el invierno");
+    break;
+
+
+}
 
 
 
