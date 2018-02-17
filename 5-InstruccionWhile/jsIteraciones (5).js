4d08d225591ebbef8c31b523e23bab05192fa587
elijanaemilyson <36230674+elijanaemilyson@users.noreply.github.com>
2018-02-17 16:45:38 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (5).js
+++ 5-InstruccionWhile/jsIteraciones (5).js
@@ -1,15 +1,24 @@
 function Mostrar()
 {
-
+//pido al usuario que declare su sexo con f o m para referirse a femenino o masculino
 var sexo = prompt("ingrese f ó m .");
 
-while (document.getElementById('Sexo').value=sexo);
-
-
+while (!(sexo == "f" || sexo == "m") ) 
 {
-    prompt("Solo ingrese F o M");
+  sexo =  prompt("Solo ingrese f o m");
 }
 
+document.getElementById("Sexo").value = sexo ; 
+
+if ( sexo == "f" )
+{
+    document.getElementById("Sexo").value = "femenino" ; 
+}
+
+else if ( sexo == "m")
+{
+    document.getElementById("Sexo").value = "masculino";
+}
 
 
 }//FIN DE LA FUNCIÓN
