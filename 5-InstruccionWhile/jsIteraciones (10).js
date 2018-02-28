95e62c6e122e0ac697a8d43c4db6391fcd7d6768
elijanaemilyson <36230674+elijanaemilyson@users.noreply.github.com>
2018-02-28 07:09:57 -0300

    terminar el 10

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -1,17 +1,48 @@
 function Mostrar()
 {
 
-	var contador=0;
-	//declarar contadores y variables 
+	var contadorNeg=0;
+	var numeros;
+	var sumaNegativo;
+	var sumaPositivo;
+	var contadorPosi=0;
+	var contadorCeros;
+	var respuesta=true;
 	
-	var respuesta="si";
+	/*Pedir numeros hasta el usuario quiera , estos datos entraran por prompt y seguira ingresando numeros por confirm
+	 por una cantidad indefinida de veces*/
 
-	while(respuesta!="no")
+	do
 	{
-		
-	
-	}
+		numeros = parseInt(prompt("Ingrese numeros para operar"));
 
+		if ( 0 > numeros ){
+			sumaNegativo = sumaNegativo + numeros;
+			contadorNeg++;
+		}
+
+		else if ( 0 < numeros ){
+			sumaPositivo = sumaPositivo + numeros;
+			contadorPosi++;
+		}
+		else{
+			contadorCeros++;
+
+		}
+		if (numeros % 2 == 0){
+			contadorPar++;
+		}
+
+
+
+		respuesta = confirm("¿Desea seguir?");
+
+	}while( respuesta == true )
+
+		document.write("La suma de los numeros negativos es = " + sumaNegativo + "<br/>");
+		document.write("La suma de los numeros positivos es = " + sumaPositivo + "<br/>");
+		document.write("Los numeros positivos son = " + contadorPosi + "<br/>");
+		document.write("Los numeros negativos son = " + contadorNeg + "<br/>");
 
 
 
