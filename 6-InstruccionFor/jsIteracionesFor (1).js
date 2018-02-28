53645ee1145f0f1eb781f382d31b29832a2e1243
elijanaemilyson <36230674+elijanaemilyson@users.noreply.github.com>
2018-02-28 07:13:10 -0300

    Add files via upload

--- 6-InstruccionFor/jsIteracionesFor (1).js
+++ 6-InstruccionFor/jsIteracionesFor (1).js
@@ -1,4 +1,9 @@
 function Mostrar()
 {
-
+//variable es 0 
+//vamos a ejecutar 10 veces
+//el contador va a contar a partir de 1 de 1 en 1
+for( i= 1; i <= 10; i++ ){
+    document.write( i + "<br>");
+}
 }
